8b55787f9cb6f83422ef5f1b68e3239e72868864
user name <wlwrl999@gmail.com>
2023-11-19 02:19:33 +0900

    debug_jws

--- Atari_Rainbow/sunrise_minigrid.py
+++ Atari_Rainbow/sunrise_minigrid.py
@@ -29,38 +29,46 @@ import wandb
 # Note that hyperparameters may originally be reported in ATARI game frames instead of agent steps
 parser = argparse.ArgumentParser(description='Rainbow')
 parser.add_argument('--id', type=str, default='boot_rainbow', help='Experiment ID')
-parser.add_argument('--seed', type=int, default=1, help='Random seed')
+parser.add_argument('--seed', type=int, default=123, help='Random seed')
 parser.add_argument('--disable-cuda', default=False, action='store_true', help='Disable CUDA')
-parser.add_argument('--game', type=str, default='MiniGrid-DistShift1-v0', help='minigrid')
-parser.add_argument('--T-max', type=int, default=int(50e6), metavar='STEPS', help='Number of training steps (4x number of frames)')
+parser.add_argument('--game', type=str, default='MiniGrid-Empty-5x5-v0', help='minigrid')
+# parser.add_argument('--T-max', type=int, default=int(50e6), metavar='STEPS', help='Number of training steps (4x number of frames)')
+parser.add_argument('--T-max', type=int, default=int(5e6), metavar='STEPS', help='Number of training steps (4x number of frames)')
 parser.add_argument('--max-episode-length', type=int, default=int(108e3), metavar='LENGTH', help='Max episode length in game frames (0 to disable)')
 parser.add_argument('--history-length', type=int, default=1, metavar='T', help='Number of consecutive states processed')
 parser.add_argument('--architecture', type=str, default='mlp', choices=['canonical', 'data-efficient'], metavar='ARCH', help='Network architecture')
-parser.add_argument('--hidden-size', type=int, default=512, metavar='SIZE', help='Network hidden size')
+# parser.add_argument('--hidden-size', type=int, default=512, metavar='SIZE', help='Network hidden size')
+parser.add_argument('--hidden-size', type=int, default=128, metavar='SIZE', help='Network hidden size')
 parser.add_argument('--noisy-std', type=float, default=0.1, metavar='σ', help='Initial standard deviation of noisy linear layers')
 parser.add_argument('--atoms', type=int, default=51, metavar='C', help='Discretised size of value distribution')
-parser.add_argument('--V-min', type=float, default=-10, metavar='V', help='Minimum of value distribution support')
-parser.add_argument('--V-max', type=float, default=10, metavar='V', help='Maximum of value distribution support')
+# parser.add_argument('--V-min', type=float, default=-10, metavar='V', help='Minimum of value distribution support')
+parser.add_argument('--V-min', type=float, default=-2, metavar='V', help='Minimum of value distribution support')
+# parser.add_argument('--V-max', type=float, default=10, metavar='V', help='Maximum of value distribution support')
+parser.add_argument('--V-max', type=float, default=2, metavar='V', help='Maximum of value distribution support')
 parser.add_argument('--model', type=str, metavar='PARAMS', help='Pretrained model (state dict)')
 parser.add_argument('--memory-capacity', type=int, default=int(1e6), metavar='CAPACITY', help='Experience replay memory capacity')
-parser.add_argument('--replay-frequency', type=int, default=4, metavar='k', help='Frequency of sampling from memory')
+# parser.add_argument('--replay-frequency', type=int, default=4, metavar='k', help='Frequency of sampling from memory')
+parser.add_argument('--replay-frequency', type=int, default=1, metavar='k', help='Frequency of sampling from memory')
 parser.add_argument('--priority-exponent', type=float, default=0.5, metavar='ω', help='Prioritised experience replay exponent (originally denoted α)')
 parser.add_argument('--priority-weight', type=float, default=0.4, metavar='β', help='Initial prioritised experience replay importance sampling weight')
 parser.add_argument('--multi-step', type=int, default=3, metavar='n', help='Number of steps for multi-step return')
 parser.add_argument('--discount', type=float, default=0.99, metavar='γ', help='Discount factor')
 parser.add_argument('--target-update', type=int, default=int(8e3), metavar='τ', help='Number of steps after which to update target network')
-parser.add_argument('--reward-clip', type=int, default=1, metavar='VALUE', help='Reward clipping (0 to disable)')
-parser.add_argument('--learning-rate', type=float, default=0.0000625, metavar='η', help='Learning rate')
+# parser.add_argument('--reward-clip', type=int, default=1, metavar='VALUE', help='Reward clipping (0 to disable)')
+parser.add_argument('--reward-clip', type=int, default=0, metavar='VALUE', help='Reward clipping (0 to disable)')
+# parser.add_argument('--learning-rate', type=float, default=0.0000625, metavar='η', help='Learning rate')
+parser.add_argument('--learning-rate', type=float, default=0.03, metavar='η', help='Learning rate')
 parser.add_argument('--adam-eps', type=float, default=1.5e-4, metavar='ε', help='Adam epsilon')
 parser.add_argument('--batch-size', type=int, default=32, metavar='SIZE', help='Batch size')
 # parser.add_argument('--learn-start', type=int, default=int(20e1), metavar='STEPS', help='Number of steps before starting training')
-parser.add_argument('--learn-start', type=int, default=int(1000), metavar='STEPS', help='Number of steps before starting training')
+parser.add_argument('--learn-start', type=int, default=int(10e3), metavar='STEPS', help='Number of steps before starting training')
 parser.add_argument('--evaluate', action='store_true', help='Evaluate only')
 # parser.add_argument('--evaluation-interval', type=int, default=200000, metavar='STEPS', help='Number of training steps between evaluations')
-parser.add_argument('--evaluation-interval', type=int, default=2000, metavar='STEPS', help='Number of training steps between evaluations')
+parser.add_argument('--evaluation-interval', type=int, default=100000, metavar='STEPS', help='Number of training steps between evaluations')
 parser.add_argument('--evaluation-episodes', type=int, default=10, metavar='N', help='Number of evaluation episodes to average over')
 # TODO: Note that DeepMind's evaluation method is running the latest agent for 500K frames ever every 1M steps
-parser.add_argument('--evaluation-size', type=int, default=500, metavar='N', help='Number of transitions to use for validating Q')
+# parser.add_argument('--evaluation-size', type=int, default=500, metavar='N', help='Number of transitions to use for validating Q')
+parser.add_argument('--evaluation-size', type=int, default=1000, metavar='N', help='Number of transitions to use for validating Q')
 parser.add_argument('--render', action='store_true', help='Display screen (testing only)')
 parser.add_argument('--enable-cudnn', action='store_true', help='Enable cuDNN (faster but nondeterministic)')
 parser.add_argument('--checkpoint-interval', default=0, help='How often to checkpoint the model, defaults to 0 (never checkpoint)')
@@ -78,10 +86,10 @@ parser.add_argument('--ucb-train', type=float, default=0.0, help='coeff for UCB 
 args = parser.parse_args()
 
 # wandb intialize
-# wandb.init(project="Sunrise_minigrid_Jws",
-#            name=args.game + str(datetime.now()),
-#            config=args.__dict__
-#            )
+wandb.init(project="Sunrise_minigrid_Jws",
+           name=args.game + str(datetime.now()),
+           config=args.__dict__
+           )
 
 
 print(' ' * 26 + 'Options')
@@ -170,7 +178,7 @@ while T < args.evaluation_size:
     state = next_state
     T += 1
 
-    # wandb.log({'Replay_memory/reward':r})
+    wandb.log({'Replay_memory/reward':r})
 
 if args.evaluate:
     for en_index in range(args.num_ensemble):
@@ -191,8 +199,8 @@ else:
         if done or truncated:
             # print episode rewards
             print('episode reward: ', env.total_rewards)
-            # wandb.log({'training/ep_reward': env.total_rewards
-            #            })
+            wandb.log({'training/ep_reward': env.total_rewards
+                       })
             state, info = env.reset(seed=42)
             done = False
             state = torch.Tensor(state).to(args.device)
@@ -233,8 +241,8 @@ else:
         done = done or truncated
         mem.append(state.unsqueeze(0), action, reward, done)  # Append transition to memory
 
-        # wandb.log({'training/reward': reward
-        #            })
+        wandb.log({'training/reward': reward
+                   })
         # Train and test
         if T >= args.learn_start:
             mem.priority_weight = min(mem.priority_weight + priority_weight_increase, 1)  # Anneal importance sampling weight β to 1
@@ -291,11 +299,11 @@ else:
                 for en_index in range(args.num_ensemble):
                     dqn_list[en_index].train()  # Set DQN (online network) back to training mode
 
-                    # wandb.log({'eval/reward': reward,
-                    #            'eval/Average_reward': avg_reward,
-                    #            'eval/timestep': T,
-                    #            'eval/Q-value': avg_Q
-                    #            })
+                    wandb.log({'eval/reward': reward,
+                               'eval/Average_reward': avg_reward,
+                               'eval/timestep': T,
+                               'eval/Q-value': avg_Q
+                               })
 
                 # If memory path provided, save it
                 if args.memory is not None:
